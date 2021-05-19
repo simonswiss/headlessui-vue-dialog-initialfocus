f57df27608eae9e1e7590bef536af2c5b6bb111a
Simon Vrachliotis <simon@simonswiss.com>
2021-05-19 17:38:29 +1000

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <button @click="setIsOpen(open)">Open Dialog</button>
+  <Dialog :open="isOpen" @close="setIsOpen(false)" :initialFocus="initialFocusRef">
+    <DialogTitle>Title</DialogTitle>
+    <DialogDescription
+      >Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio consectetur, vero quo laboriosam eum inventore voluptate optio
+      doloribus, odit illo quos itaque harum nulla soluta similique, explicabo rem veniam quis?</DialogDescription
+    >
+    <button @click="setIsOpen(false)">Cancel</button>
+    <button ref="initialFocusRef" @click="setIsOpen(false)">Do it!</button>
+  </Dialog>
+</template>
+
+<script>
+import { ref } from "vue";
+
+import { Dialog, DialogTitle, DialogDescription } from "@headlessui/vue";
+
+export default {
+  components: {
+    Dialog,
+    DialogTitle,
+    DialogDescription,
+  },
+  setup() {
+    const isOpen = ref(false);
+    const initialFocusRef = ref(null);
+
+    return {
+      isOpen,
+      initialFocusRef,
+      setIsOpen(value) {
+        isOpen.value = value;
+      },
+    };
+  },
+};
+</script>
